2fef69c74e2947061e2c1cef82c8a44f031f69b6
Dirk Heilig <dirk@heilig-online.com>
2018-02-19 22:39:29 +0100

    ass shellcheck test

--- shpec/lag_shpec.sh
+++ shpec/lag_shpec.sh
@@ -4,19 +4,19 @@ describe "the Latest And Greatest"
     it "should always run the latest and greatest version of your tool"
         assert equal "$(./src/lag ./testcases/foo-*-bar/helloworld)" "hello it's me ./testcases/foo-0.10.0-bar/helloworld"
     end
-    it "should pollute your \$path"
+    it 'should pollute your $path'
         LAG_CONFFILE=./testcases/conf ./src/lag pollute ./testcases/bin
         OUTPUT="$(LAG_CONFFILE=./testcases/conf ./testcases/bin/hello)"
         assert equal "$OUTPUT" "hello it's me ./testcases/foo-0.10.0-bar/helloworld"
     end
     it "should take care of your params"
         LAG_CONFFILE=./testcases/conf ./src/lag pollute ./testcases/bin
-        OUTPUT="$(LAG_CONFFILE=./testcases/conf ./testcases/bin/hello 1 2 3 "4 5"  6)"
+        OUTPUT="$(LAG_CONFFILE=./testcases/conf ./testcases/bin/hello 1 2 3 "4 5" 6)"
         assert equal "$OUTPUT" "hello it's me ./testcases/foo-0.10.0-bar/helloworld,1,2,3,4 5,6"
-
+    end
+    it "should pass shellcheck"
+        shellcheck ./src/lag
+        assert equal "$?" 0
     end
 end
 rm -rf testcases
-
-
-
